02a827f2bbbbd1ca7cbdab7f9a600eb4d1aeb042
anthonyburdi <anthonyburdi@gmail.com>
2015-05-01 14:55:19 -0400

    added provision script and requirements.txt

---
+++ provision.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+## Update and Install Dependencies
+apt-get update
+apt-get upgrade -y
+apt-get install -y git
+apt-get -y install python-pip
+pip install virtualenv
+
+## Set Up Virtual Environment, Install Dependencies
+cd /vagrant
+virtualenv venv
+source venv/bin/activate
+pip install -r /vagrant/requirements.txt
+
+## Start Flask App
+# python mytoday.py
